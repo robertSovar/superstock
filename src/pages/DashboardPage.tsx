a4616bdfc72aca7a1611bcc2d8c8c1ae4682ddf6
robertSovar <133514926+robertSovar@users.noreply.github.com>
2025-01-10 21:58:51 +0200

    Update proj

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,22 @@
+import { CiBoxList } from "react-icons/ci";
+import { FaWpforms } from "react-icons/fa";
+import { LiaFileInvoiceSolid } from "react-icons/lia";
+import { LuUsers } from "react-icons/lu";
+
 function DashboardPage() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
+  return (
+    <section className="h-full flex min-h-screen w-screen">
+      <div>
+        <ul className="flex-col gap-15 pt-6 px-6 height-full w-30 bg-red-100 min-h-screen ">
+          <li className="flex "> {<CiBoxList />}Equipments</li>
+          <li className="flex">{<FaWpforms />}Orders</li>
+          <li className="flex">{<LiaFileInvoiceSolid />}Invoices</li>
+          <li className="flex">{<LuUsers />}Users</li>
+        </ul>
+      </div>
+      <div className="border "></div>
+    </section>
+  );
 }
 
 export default DashboardPage;
