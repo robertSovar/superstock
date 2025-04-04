a26bbd64eee9613e5d0b4acc568db206443e8d2d
Sovar Robert Ionut <sovar_robert_ionut@yahoo.com>
2025-04-04 18:07:41 +0300

    Update proj

--- src/components/EditCardDropdown/EditCardDropdown.tsx
+++ src/components/EditCardDropdown/EditCardDropdown.tsx
@@ -1,10 +1,10 @@
 const EditCardDropdown = () => {
   return (
     <div className="flex flex-col absolute right-[2px] ">
-      <ul className="flex flex-col gap-1  text-xs">
-        <li>Edit</li>
-        <li>Delete</li>
-      </ul>
+      <div className="flex flex-col gap-1  text-xs">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
   );
 };
---
+++ src/components/Forms/EditEquipmentForm.tsx
@@ -0,0 +1,9 @@
+const EditEquipmentForm = () => {
+  return (
+    <div>
+      <h1>EditEquipmentForm</h1>
+    </div>
+  );
+};
+
+export default EditEquipmentForm;
